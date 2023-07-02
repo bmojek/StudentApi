fe7b5f3bb8974983c81771e0b46e6681a8e733a7
Bartłomiej Mojek <Bartlomiej.Mojek@microsoft.wsei.edu.pl>
2023-07-02 19:37:30 +0200

    Dodaj pliki projektów.

---
+++ StudentApi/Models/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentApi.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; } = null!;
+    }
+}
